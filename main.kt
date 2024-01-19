5dbb51f02e832b6457856f1481d5541f8ffaac3f
Drew Howells <drewjhowells@gmail.com>
2024-01-18 21:50:26 -0700

    Wrote out commented shell for task manager

--- main.kt
+++ main.kt
@@ -1,5 +1,26 @@
+//Define Task data type
+data class task {
 
+}
 
-fun main(args: Array<String>) {
+//Define Task Manager class w/ methods to add tasks and mark completed
+class taskManager {
+    fun addTask {
 
     }
+    fun markCompleted {
+
+    }
+
+}
+
+//Run Task manager in console w/ user input
+fun main(args: Array<String>) {
+
+
+    while (true)
+    {
+        //Write out new screen and show tasks by due date
+
+    }
+}
