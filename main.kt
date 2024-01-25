8c8a15daeb25771d5350d8a37912302ed6ea3ede
Drew Howells <drewjhowells@gmail.com>
2024-01-25 10:18:21 -0700

    Debugged broken methods, fixed

--- main.kt
+++ main.kt
@@ -1,66 +1,71 @@
 //Define Task data type
-data class Task(val name: String) {
-    private var _id: Int = 0
+data class Task(val name: String, val id: Int) {
+    var _id: Int = id
     var taskName = name
-    var id: Int
-        get() = _id
-        private set(value) {
-            _id = value
-        }
-    init {
-        _id++
-    }
-    fun get_id(): Int {
-        return id
-    }
 }
 
 
 //Define Task Manager class w/ methods to add tasks and mark completed
 class taskManager() {
     val taskList = mutableListOf<Task>()
+    var id: Int = 1
+
     fun listTasks() {
-        for (task in taskList)
-            println(task.get_id())
+        println("\nTasks:")
+        for (i in 0 .. taskList.count()-1) {
+            println(taskList[i]._id.toString() + ": " + taskList[i].taskName)
+        }
+
     }
+
     fun addTask (name: String){
-        val task = Task(name)
+        val task = Task(name, id)
+        taskList.add(task)
+        id++
     }
     fun setTaskName(): String {
         print("Task name: ")
-        val name :String = try {
-            readLine().toString()
-        } catch (e: Exception) {
-            println("Invalid Entry")
-            setTaskName()
-        }
+        val name :String = readLine()!!.toString()
         return name
     }
+
+    fun deleteTask() {
+        print("Task ID: ")
+        val task_id :Int = readLine()!!.toInt()
+        taskList.removeAt(task_id-1)
+        println("Task Deleted!")
+        for (i in task_id-1 .. taskList.count()-1) {
+            taskList[i]._id --
+        }
+        id--
+    }
+
 }
 
 //Run Task manager in console w/ user input
 fun main(args: Array<String>) {
     val taskManager = taskManager()
-
-    while (true) {
+    var going = true
+    while (going) {
         //Write out tasks for user to view
         taskManager.listTasks()
 
         //Query user for input
-        println("Menu Options\n1: Add Task\n2:Mark Task Completed")
+        print("Menu Options\n1) Add Task\n2) Mark Task Completed\n3) Exit\n> ")
         val input = readLine()!!.toInt()
 
-        //If input is 1, create new task, if 2 delete a task
+        //If input is 1, create new task, if 2 delete a task, if 3 exit
         if (input == 1)
         {
             taskManager.addTask(taskManager.setTaskName())
+            println("Task Added!")
         } else if (input == 2) {
-
+            taskManager.deleteTask()
+        } else if (input == 3){
+            going = false
+            println("Goodbye!")
         } else {
             println("Invalid Entry")
         }
-        //If input is 2, query for which task and delete it
-
     }
-
 }
